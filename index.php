fe7bb2088adb09a4947ee09b51d18d58a9bb52b1
crudfifth <crudfifth@gmail.com>
2024-08-01 16:32:10 +0900

    test

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/php/layout/main.php'); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/php/layout/footer.php'); ?>
     <script type="text/javascript" src="<?= '/js/main.js'; ?>"></script>
+    <script></script>
   </body>
 </html>
